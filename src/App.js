fbab8c9d97d7c71a04d9b8a657cc083b29f09f3d
creativeorange3 <creativeorange3@gmail.com>
2022-07-12 23:37:45 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.scss";
+import Radio from "./Radio";
+
+function App() {
+  return (
+    <div className="App">
+      <div><img src="./logo.png" style={{ width: "200px" }} /></div>
+      <h2>Radio Aggregator</h2>
+      <Radio />
+    </div>
+  );
+}
+
+export default App;
